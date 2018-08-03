91d25b9e4d4bc6402d38847d41655f7b15086823
Adam <ins1gn1a@users.noreply.github.com>
2018-08-03 17:07:53 +0100

    Update adXtract.sh

--- adXtract.sh
+++ adXtract.sh
@@ -3,23 +3,41 @@ printf "\33c"
 
 if [ -z $3 ]; then
 
-        echo 'usage: /location of/ntds.dit /location of/SYSTEM ProjectName';
+        echo '[!] Usage: ntds.dit SYSTEM ProjectName';
+        echo '[!] Use full paths (not relative) for NTDS and SYSTEM files';
 
         exit 1;
 
 fi
+
 CWD="$(pwd)"
-echo 'the working dir is ' $CWD
-mkdir $CWD'/adXtract_'$3
-mkdir $CWD'/adXtract_'$3'/Maps'
-cd $CDW resources/libesedb-20160622/esedbtools
-echo 'This could take a while... time for coffee!'
-./esedbexport -t $CWD/adXtract_$3/$3 $1 > $CWD/adXtract_$3/exported
-cd ../../
+NTDS=$1
+SYSFILE=$2
+PROJECT=$3
+
+echo '[-] Working dir: ' $CWD
+
+mkdir $CWD'/projects_'$PROJECT
+mkdir $CWD'/projects_'$PROJECT'/Maps'
+
+cd resources/libesedb-20160622/esedbtools
+
+echo '[-] Extracting tables'
+./esedbexport -t $CWD/projects_$PROJECT/$PROJECT $NTDS > $CWD/projects_$PROJECT/exported.log
+
 cd $CWD 
-echo $CWD
+echo "[-] Moved to dir: "$CWD
+
 cd resources/ntdsxtract2/
-./dsusers.py $CWD/adXtract_$3/$3.export/datatable* $CWD/adXtract_$3/$3.export/link_table* $CWD/adXtract_$3/Maps/ --passwordhashes --pwdformat ophc --passwordhistory --syshive $2 --lmoutfile $CWD/adXtract_$3/$3_allLMhashes.txt --ntoutfile $CWD/adXtract_$3/$3_allNTLMhashes.txt --csvoutfile $CWD/adXtract_$3/$3_UserAccountOut.csv
-./dsgroups.py $CWD/adXtract_$3/$3.export/datatable* $CWD/adXtract_$3/$3.export/link_table* $CWD/adXtract_$3/Maps/ --members --csvoutfile $CWD/adXtract_$3/$3_GroupMembershipOut.csv
-cat $CWD/adXtract_$3/$3_allNTLMhashes.txt
-echo "$CWD/adXtract."$3"/"$3"allLMhashes.txt"
+
+echo "[-] Extracting LM and NTLM hashes"
+./dsusers.py $CWD/projects_$PROJECT/$PROJECT.export/datatable* $CWD/projects_$PROJECT/$PROJECT.export/link_table* $CWD/projects_$PROJECT/Maps/ --passwordhashes --pwdformat ophc --passwordhistory --syshive $SYSFILE --lmoutfile $CWD/projects_$PROJECT/$PROJECT_allLMhashes.txt --ntoutfile $CWD/projects_$PROJECT/$PROJECT_allNTLMhashes.txt --csvoutfile $CWD/projects_$PROJECT/$PROJECT_UserAccountOut.csv
+
+echo "[-] Extracting Group Membership information"
+./dsgroups.py $CWD/projects_$PROJECT/$PROJECT.export/datatable* $CWD/projects_$PROJECT/$PROJECT.export/link_table* $CWD/projects_$PROJECT/Maps/ --members --csvoutfile $CWD/projects_$PROJECT/$PROJECT_GroupMembershipOut.csv
+
+echo "[+] Completed: "
+echo "  LM Hashes        : $CWD/projects_"$PROJECT"/"$PROJECT"_allLMhashes.txt"
+echo "  NTLM Hashes      : $CWD/projects_"$PROJECT"/"$PROJECT"_allNTLMhashes.txt"
+echo "  User Accounts    : $CWD/projects_"$PROJECT"/"$PROJECT"_UserAccountOut.csv"
+echo "  Group Membership : $CWD/projects_"$PROJECT"/"$PROJECT"_GroupMembershipOut.csv"
