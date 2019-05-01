a506cf78f991f69aaefd0dc4c932179102a1e156
LordNem <lordn3mesis@hotmail.com>
2019-05-01 14:53:00 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 CWD="$(pwd)"
-cd resources/
-tar xf libesedb-20160622.tar.gz
-cd libesedb-20160622
-./configure
-make
+sudo apt-get install libesedb-dev
 printf "\033c"
 echo "Configuration has completed"
 echo "run adXtract.sh /pathto/ntds.dit /pathto/SYSTEM /ProjectName"
